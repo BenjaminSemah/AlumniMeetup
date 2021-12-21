e92045fea574481e5dbebd46a24ef5f050e75b21
Benjamin Semah <benjaminsemah@gmail.com>
2021-12-21 00:01:17 +0000

    Create dynamic HTML with JavaScript for speakers section

--- interactions.js
+++ interactions.js
@@ -1,10 +1,86 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
+/////////////// SPEAKERS SECTION - DYNAMIC HTML ///////////////
+
+const speakersInfo = [
+  {
+    img: './images/speakers/speaker1.png',
+    speakerName: 'Omo Akuba Yalley',
+    speakerPortfolio: 'Founder, TheBrightDev.com',
+    speakerBrief: 'Omo is passionate about connecting devs with information needed to make intelligent decisions.',
+  },
+  {
+    img: './images/speakers/speaker2.png',
+    speakerName: 'Ann-Marie Adjoba',
+    speakerPortfolio: 'Microverse Alumnus',
+    speakerBrief: 'Adjoba is a recognized tech consultant and spent 3 years as a Strategic Planning Consultant.',
+  },
+  {
+    img: './images/speakers/speaker3.png',
+    speakerName: 'Eddie O. Agyeman',
+    speakerPortfolio: 'Managing Director, MeTo Stores',
+    speakerBrief: 'Eddie is an award-winning tech executive and change leader.',
+  },
+  {
+    img: './images/speakers/speaker4.png',
+    speakerName: 'Theophilus Annor',
+    speakerPortfolio: 'CTO, Anbuzz Technologies',
+    speakerBrief: 'Theophillus is the recipient of numerous awards for his outstanding leadership.',
+  },
+  {
+    img: './images/speakers/speaker5.png',
+    speakerName: 'Teresa Agyiriba',
+    speakerPortfolio: 'Team Lead, Tech Recruiters Hub',
+    speakerBrief: 'Agyiriba has worked as tech recruiter for five years. She is also founder of TechNewbies.',
+  },
+  {
+    img: './images/speakers/speaker6.png',
+    speakerName: 'Frank Piedu',
+    speakerPortfolio: 'Microverse Alumnus',
+    speakerBrief: 'Frank has seven years of experience as a Freelance Front End Developer',
+  },
+];
+
+function fetchSpeakers(s) {
+  const speakerHTML = `
+  <li class="speaker-card">
+    <img
+      class="speaker-image"
+      src="${s.img}"
+      alt="name of Speaker"
+    />
+    <div class="speaker-info">
+      <h4 class="speaker-name">${s.speakerName}</h4>
+      <p class="speaker-portfolio">${s.speakerPortfolio}</p>
+      <div class="speaker-info-line"></div>
+      <p class="speaker-brief">
+        ${s.speakerBrief}
+      </p>
+    </div>
+  </li>`
+  return speakerHTML;
+}
+
+const speakersList = document.querySelector('.speakers-list');
+
+function addSpeakers () {
+  let dynamicSpeakers = '';
+  speakersInfo.forEach((s) => {
+    dynamicSpeakers = dynamicSpeakers + fetchSpeakers(s); 
+  });
+  speakersList.innerHTML = dynamicSpeakers; 
+}
+
+speakersList.onresize = addSpeakers();
+
+/////////////// MOBILE MENU INTERACTIONS ///////////////
+
+const hamburgerMenu = document.querySelector('#hmenu1');
 const menuCloseBtn = document.querySelector('#mobile-menu-close');
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuHome = document.querySelector('.mobile-menu-home');
 const mobileMenuAbout = document.querySelector('.mobile-menu-about');
 const mobileMenuProgram = document.querySelector('.mobile-menu-program');
 const mobileMenuSponsors = document.querySelector('.mobile-menu-sponsors');
+const hMenu = document.querySelector('#hmenu2');
 
 function openMobileMenu() {
   mobileMenu.style.display='block';
@@ -20,3 +96,4 @@ mobileMenuHome.addEventListener('click', closeMobileMenu);
 mobileMenuAbout.addEventListener('click', closeMobileMenu);
 mobileMenuProgram.addEventListener('click', closeMobileMenu);
 mobileMenuSponsors.addEventListener('click', closeMobileMenu);
+hMenu.addEventListener('click', openMobileMenu);
